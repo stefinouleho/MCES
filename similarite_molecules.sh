0d8ada830680ce270278dc73ce8fbe3528be13c9
stefinouleho <stefinouleho@users.noreply.github.com>
2017-07-26 16:25:37 +0200

    Add files via upload

---
+++ similarite_molecules.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#  similarite_molecules.sh
+#  
+#
+#  Created by Stefi Nouleho on 26/07/2017.
+#
+
+# calcul de la similarite des molecules se trouvant dans le fichier molecules.txt
+
+fichiermol="molecules.txt"
+while read chebiid temps taille; do
+
+    fichier="resultats/similarite_"$chebiid"_"$temps"_"$taille"_all.data"
+    if [ ! -f "$fichier" ]; then
+        echo "$chebiid $temps $taille"
+        ./mesure_similarite $chebiid 0 $temps $taille
+    fi
+done <"$fichiermol"
